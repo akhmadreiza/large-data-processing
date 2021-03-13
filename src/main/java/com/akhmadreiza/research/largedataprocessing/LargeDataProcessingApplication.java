1f624b1fb0b83abf9317eefcd05eb6ccc6632e41
Akhmad Reiza <reizaarmando@gmail.com>
2021-03-13 11:06:38 +0700

    initial commit project

---
+++ src/main/java/com/akhmadreiza/research/largedataprocessing/LargeDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.akhmadreiza.research.largedataprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeDataProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhmadreiza/research/largedataprocessing/LargeDataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhmadreiza.research.largedataprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeDataProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
