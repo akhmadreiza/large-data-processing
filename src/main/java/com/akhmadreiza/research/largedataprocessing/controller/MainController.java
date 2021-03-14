83c0a04e82967591ae5a22314cf588b605ecd84e
Akhmad Reiza <reizaarmando@gmail.com>
2021-03-14 13:11:32 +0700

    remove write response to log

--- src/main/java/com/akhmadreiza/research/largedataprocessing/controller/MainController.java
+++ src/main/java/com/akhmadreiza/research/largedataprocessing/controller/MainController.java
@@ -4,9 +4,6 @@ import com.akhmadreiza.research.largedataprocessing.constants.DataProcessorType;
 import com.akhmadreiza.research.largedataprocessing.domain.MasterTableDto;
 import com.akhmadreiza.research.largedataprocessing.service.DataProcessorFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +14,6 @@ import java.util.List;
 
 @RestController
 public class MainController {
-
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     @Autowired
     private DataProcessorFactory dataProcessorFactory;
@@ -42,7 +35,6 @@ public class MainController {
     private ResponseEntity<String> getStringResponseEntity(long startTime, List<MasterTableDto> result) throws JsonProcessingException {
         long finishTime = System.currentTimeMillis();
         long timeTaken = finishTime - startTime;
-        LOGGER.info(objectMapper.writeValueAsString(result));
         return new ResponseEntity<>("OK (row selected: " + result.size() + " timeTaken: " + timeTaken + " ms)", HttpStatus.OK);
     }
 }
